341bb354972e3328ca6425e2e9b575fca517c50f
Hanan Mohamud <152763595+hananmyy@users.noreply.github.com>
2024-08-23 13:01:25 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,8 @@
+#Define the Function(a function is a reusable piece of code).
+#return is a way of outputting but you can define how you want your output to look
+def greet(person):
+    return f"Good morning, {person}!"
+
+#Applying the function
+print(greet("Alice"))
+print(greet("Hanan"))
---
+++ intro.py
@@ -0,0 +1,34 @@
+#let's store a favourite number in a variable
+# Favourite_Number = 73
+# print(f"My favourite number is: {Favourite_Number}")
+
+# #String 
+# greeting = "Hello, PLP Academy"
+# print(greeting)
+
+# #Boolean represents a situation where there is true or false
+# is_python_fun = True
+# is_python_boring = False
+# print(f'Is Python fun? {is_python_fun}')
+# #f is used to recall a function
+# print(f'Is Python boring? {is_python_boring}')
+
+# #Control_Flow
+# temperature = 30
+# if temperature > 25:
+#     print ("It's hot outside! Wear Shorts.'")
+# elif temperature > 15:
+#     print ("It's warm. Maybe wear a light jacket.")
+# else:
+#     print("Brrrr! It's cold. Bundle Up!")
+
+# #Module- it's a file that contains functions that are reusable
+# #In-Built Module
+# import math
+# result = math.sqrt(144)
+# print(f'The square root of 144 is {result}')
+
+#Making a Customized Module
+import greetings
+print(greetings.greet("Adelaide"))
+print(greetings.greet("Umi"))
